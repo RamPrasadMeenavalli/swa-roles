d4f660858313036dcb968d8eaf851f189648eef2
Ram Prasad Meenavalli <rmeenavalli@microsoft.com>
2023-04-07 23:56:09 +0530

    Added AAD config

--- api/GetRoles/index.js
+++ api/GetRoles/index.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch').default;
 
 // add role names to this object to map them to group ids in your AAD tenant
 const roleGroupMappings = {
-    'admin': '21a96550-aa02-486e-9297-e6e51b6398fc',
+    'admin': 'be8ca378-bc74-46c1-b922-e7f552486ede',
     'reader': '33bb071c-118d-40d1-a5d7-7ced5900b973'
 };
 
