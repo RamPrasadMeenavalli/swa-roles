0d525443a8748ee3506d15ba3b4a223c093aab38
Ram <ram@meenavalli.in>
2023-04-10 18:28:39 +0530

    reverted oaut 2.0

--- api/activate/index.js
+++ api/activate/index.js
@@ -1,11 +1,19 @@
 module.exports = async function (context, req) {
     const header = req.headers['x-ms-client-principal'];
+    const token = req.headers['x-ms-token-aad-access-token'];
     const encoded = Buffer.from(header, 'base64');
     const decoded = encoded.toString('ascii');
-  
+
+    //const tokenencoded = Buffer.from(token, 'base64');
+    //const tokendecoded = tokenencoded.toString('ascii');
+
+    context.log("Printing headers!!");
+    context.log(req.headers);
+
     context.res = {
       body: {
         clientPrincipal: JSON.parse(decoded),
+        token: token + "!!"
       },
     };
 }
